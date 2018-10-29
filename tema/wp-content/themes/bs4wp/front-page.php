e1b76c417d9e2fb925ca9afac6532e8e7aa6357e
Ricardo Sanches <ricardo.sanches@live.com>
2018-10-28 22:09:02 -0400

    Adição do carrossel de imagens

--- tema/wp-content/themes/bs4wp/front-page.php
+++ tema/wp-content/themes/bs4wp/front-page.php
@@ -1,6 +1,63 @@
 <?php get_header(); ?>
 
       <div class="row">
+      
+        <div class="col mb-5">
+        
+          <div id="carouselBSWP" class="carousel slide" data-ride="carousel">
+          
+            <div class="carousel-inner">
+            
+              <?php 
+              // args
+              $my_args_banner = array(
+                'post_type' => 'banners',
+                'posts_per_page' => 3,
+              );
+
+              // query
+              $my_query_banner = new WP_Query ( $my_args_banner );
+              ?>
+
+              <?php if( $my_query_banner->have_posts()) : 
+                $banner = $banners[0];
+                $c = 0;
+                while( $my_query_banner->have_posts() ) : 
+                $my_query_banner->the_post(); 
+              ?>
+
+                <div class="carousel-item <?php $c++; if($c == 1) { echo ' active'; } ?>">
+                  <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded')) ?>
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>
+                      <?php the_title(); ?>
+                    </h5>
+                  </div>
+                </div>
+
+              <?php endwhile; endif; ?>
+
+              <?php wp_reset_query(); ?>
+            
+            </div>
+
+            <a class="carousel-control-prev" href="#carouselBSWP" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+              <span class="sr-only">Anterior</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carouselBSWP" role="button" data-slide="next">
+              <span class="carousel-control-next-icon"></span>
+              <span class="sr-only">Próximo</span>
+            </a>
+          
+          </div>
+        
+        </div>
+      
+      </div>
+
+      <div class="row">
 
         <?php 
         // args
--- tema/wp-content/themes/bs4wp/functions.php
+++ tema/wp-content/themes/bs4wp/functions.php
@@ -107,6 +107,28 @@ function format_comment($comment, $args, $depth) {
 
 }
 
+// Criar o tipo de post para o banner
+function create_post_type() {
+
+    register_post_type('banners',
+    // Definir as opções
+    array(
+        'labels' => array(
+            'name' => __('Banners'),
+            'singular_name' => __('Banners')
+        ),
+        'supports' => array(
+            'title', 'editor', 'thumbnail'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-images-alt2',
+        'rewrite' => array('slug' => 'banners'),
+    ));
+}
+//Iniciar o tipo de post
+add_action('init', 'create_post_type');
+
 // Incluir as funções de personalização
 require get_template_directory(). '/inc/customizer.php';
 
