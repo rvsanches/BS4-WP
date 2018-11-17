75c584e817eee81d8b1ef4f969da7f7bbabddc76
Ricardo Sanches <ricardo.sanches@live.com>
2018-11-17 10:48:54 -0300

    Atualizado para o Gulp 4

--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -1,3 +1,23 @@
+// Abaixo com a versão Gulp 4
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', gulp.series( function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('src/css'));
+}));
+
+gulp.task('watch', gulp.series( function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel( ['sass'] ));
+}));
+
+gulp.task('default', gulp.series( ['sass', 'watch'] ));
+
+
+// Abaixo com a versão Gulp 3
+/*
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
@@ -8,7 +28,8 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass'] );
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch'] );
+*/
